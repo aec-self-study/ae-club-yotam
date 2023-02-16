3a55063a90caf42b9d9cbce20def662515af4110
ydamit <29988641+ydamit@users.noreply.github.com>
2023-02-16 19:20:39 +0000

    rename example models and force table materialization

---
+++ coffee_shop_yotam/models/example/customers.sql
@@ -0,0 +1,13 @@
+{{ config(materialized='table') }}
+
+SELECT o.customer_id
+, c.name
+, c.email
+, MIN(o.created_at) AS first_order_at
+, COUNT(*) AS number_of_orders
+FROM analytics-engineers-club.coffee_shop.customers AS c
+JOIN analytics-engineers-club.coffee_shop.orders AS o
+ON o.customer_id = c.id
+GROUP BY 1, 2, 3
+ORDER BY first_order_at
+LIMIT 5
